c3e55ed5c8814b7c8b157ab574561f93e2290d95
Alan Kraszewski <113364668+krasz47@users.noreply.github.com>
2022-12-31 20:35:06 +0000

    Deleted void endtime()

--- main.cpp
+++ main.cpp
@@ -20,15 +20,10 @@ void addAlcohol() {
     AlcoholCounter += temp;
 }
 
-void endTime() {
-    std::cout << std::setprecision(2);
-    std::cout << "Time till alcohol fully out of system: " << AlcoholCounter / 10;
-}
-
 int main() {
     std::string option{};
     while (true) {
-        std::cout << "Units:\t\t" << AlcoholCounter / 10 << "\nTimeLeft:\t" << endTime() <<
+        std::cout << "Units:\t\t" << AlcoholCounter / 10 << "\nTimeLeft:\t" << AlcoholCounter/10 <<
             "\n=-=-=-=-=-=-=-=" << "\"add\"\t\t add alcohol to count\n\"sub\" subtract alcohol from count";
 
         std::cout << "Enter option: ";
